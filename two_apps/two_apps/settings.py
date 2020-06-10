e5268e5b252a1df28e3166fc32794b8ef6184753
WoodComb <63362810+WoodComb@users.noreply.github.com>
2020-06-10 10:45:14 +0530

    Update settings.py

--- two_apps/two_apps/settings.py
+++ two_apps/two_apps/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'neppo766d$^sl7634est4i=&ws#a)^in*tm1au8jvfb#ao_7qz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
